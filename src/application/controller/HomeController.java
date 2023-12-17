56a1f415b5b284560a58904cfc37a3319d343bc2
LinhBP <ygchsohcy@gmail.com>
2023-12-17 11:46:19 +0700

    Add search function

--- src/application/controller/HomeController.java
+++ src/application/controller/HomeController.java
@@ -2,39 +2,33 @@ package application.controller;
 
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.Iterator;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import application.models.Blog;
-import application.models.Collection;
 import application.views.ViewFactory;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
-import javafx.scene.Parent;
 import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
-import javafx.scene.control.Menu;
-import javafx.scene.control.TableView;
-import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.ImageView;
 import javafx.scene.image.Image;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
-import javafx.scene.text.TextAlignment;
-import javafx.stage.Stage;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
 import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
 
 public class HomeController implements Initializable{
@@ -151,6 +145,69 @@ public class HomeController implements Initializable{
 		}
 		return vBoxList;
 	}
-	
-	
+
+	public static boolean containsKeyword(String[] baseDataArray, String keyword) {
+		// Convert the keyword to lowercase for case-insensitive comparison
+		String lowercasedKeyword = keyword.toLowerCase();
+
+		// Iterate through each string in the array
+		for (String baseData : baseDataArray) {
+			// Convert the current base data to lowercase for comparison
+			String lowercasedBaseData = baseData.toLowerCase();
+
+			// Check if the lowercased base data contains the lowercased keyword
+			if (lowercasedBaseData.contains(lowercasedKeyword)) {
+				return true; // Return true if any string contains the keyword
+			}
+		}
+
+		return false; // Return false if none of the strings contain the keyword
+	}
+
+//	!If limit is 0, will return all data, if limit !=0. Will try to return number you want from result found
+	public List<JsonNode> getData(String keyword, int limit) {
+		List<JsonNode> resultNodes = new ArrayList<>();
+		File jsonFile = new File("src/resources/data/data.json");
+
+		try {
+			ObjectMapper objectMapper = new ObjectMapper();
+			JsonNode rootNode = objectMapper.readTree(jsonFile);
+
+			// Assuming your JSON file is an array, iterate over each object
+			for (JsonNode objectNode : rootNode) {
+				// Access individual fields of each object
+				String postTitle = objectNode.get("post_title").asText();
+				String postContent = objectNode.get("post_content").asText();
+				String postDate = objectNode.get("post_date").asText();
+				String postLink = objectNode.get("post_link").asText();
+				String postDescription = objectNode.get("post_description").asText();
+
+				String[] baseDataNeedCheck = {postTitle, postContent, postDescription};
+				boolean checkCondition = containsKeyword(baseDataNeedCheck, keyword);
+
+				if (checkCondition) {
+					resultNodes.add(objectNode);
+
+					// Check if the limit is reached
+					if (limit > 0 && resultNodes.size() >= limit) {
+						break;
+					}
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return resultNodes;
+	}
+	public static void main(String[] args) {
+		HomeController test1 = new HomeController();
+		List<JsonNode> resultNodes = test1.getData("", 0);
+
+		// Process or return the resultNodes as needed
+		for (JsonNode resultNode : resultNodes) {
+//			resultNode.get + name of field you want to get and print data
+			System.out.println(resultNode.get("post_title"));
+		}
+	}
 }
