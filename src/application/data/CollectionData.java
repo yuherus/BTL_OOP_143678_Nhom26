3663a14b92a662918cfaa44250cac3aa313308ae
VMH2k3 <vmh300603@gmail.com>
2023-12-23 01:54:29 +0700

    upload function Search collection

--- src/application/data/CollectionData.java
+++ src/application/data/CollectionData.java
@@ -3,6 +3,7 @@ package application.data;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.io.File;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
@@ -19,11 +20,14 @@ import javafx.scene.image.Image;
 
 public class CollectionData {
 	public static void main(String[] args) {
-		List<Collection> trendingCollections= getTrendingCollections("POLYGON", "H1", 10);
-		for (Collection collection : trendingCollections) {
-			System.out.println(collection.toString());
-		}
-		
+//		List<Collection> trendingCollections= getTrendingCollections("POLYGON", "H1", 10);
+//		for (Collection collection : trendingCollections) {
+//			System.out.println(collection.toString());
+//		}
+		List<Collection> searchCollections = getCollectionBySearchWord("Other", 5, "D1");
+		for (Collection collection : searchCollections) {
+		System.out.println(collection.toString());
+	}
 	}
 	
 
@@ -58,5 +62,48 @@ public class CollectionData {
             return trendingCollections;
         }
 	}
-	
+	public static List<Collection> getCollectionBySearchWord(String SearchWord, int limit, String period ){
+		List<Collection> collectList = new ArrayList<>();
+		File jsonFile = new File("./src/resources/data/collection"+period+".json");
+		try {
+			ObjectMapper objectMapper = new ObjectMapper();
+			JsonNode rootNode = objectMapper.readTree(jsonFile);
+
+			// Assuming your JSON file is an array, iterate over each object
+			for (JsonNode objectNode : rootNode) {
+				// Access individual fields of each object
+				String id = objectNode.get("id").asText();
+				double volumeNative = objectNode.get("volumeNative").asDouble();
+				String volumeNativeUnit = objectNode.get("volumeNativeUnit").toString();
+				double floorPrice = objectNode.get("floorPrice").asDouble();
+				String floorPriceUnit = objectNode.get("floorPriceUnit").toString();
+				double VolumeChange = objectNode.get("VolumeChange").asDouble();
+				String blockchain = objectNode.get("blockchain").toString();
+				String name = objectNode.get("name").toString();
+				String description = objectNode.get("description").toString();
+				String imageUrl;
+				if (objectNode.get("imageUrl").get(0) != null) {
+				imageUrl = objectNode.get("imageUrl").get(0).asText();
+				} else {
+				imageUrl = "/resources/images/Hinh-Nen-Trang-10.jpg";
+				}
+				String[] baseDataNeedCheck = {name, blockchain, volumeNativeUnit,description};
+				for(String DNC:baseDataNeedCheck) {
+					boolean checkCondition = DNC.contains(SearchWord);
+					if (checkCondition) {
+						collectList.add(new Collection(id, volumeNative, volumeNativeUnit, floorPrice, floorPriceUnit, VolumeChange, blockchain, name, description, imageUrl));
+						break;
+					}
+				}
+				if (limit > 0 && collectList.size() >= limit) {
+					break;
+				}
+
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return collectList;
+
+	}	
 }
